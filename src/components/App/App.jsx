bbb933a9c396c527e4865b2ccca0c75a2096efcb
dvk <dmitriykolesnyk@gmail.com>
2025-11-02 20:25:05 +0200

    task_1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,7 @@
 import Profile from "../Profile/Profile.jsx";
+import userData from "../../userData.json";
 
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
-
-const App = () => {
+export default function App() {
   return (
     <>
       <Profile
@@ -24,4 +13,4 @@ const App = () => {
       />
     </>
   );
-};
+}
